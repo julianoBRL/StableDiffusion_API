a42b2f6158fcdb794deab074cb98bd6d280fbddc
Shintaro <Shintaro0dev@gmail.com>
2022-10-09 19:46:07 -0300

    fixed error on autobalancer and auto downloader

--- src/services/StableDiffusion.py
+++ src/services/StableDiffusion.py
@@ -15,7 +15,7 @@ DEFAULT_IMG_QTD = 4
 class StableDiffusion:
     
     #Always direct the job to the gpu that has more memory available
-    def gpu_loadbalance():
+    def gpu_loadbalance(self):
         list_available_memory = []
         for i in range(torch.cuda.device_count()):
             list_available_memory.append(round(torch.cuda.memory_reserved(i)/1024**3,1))
@@ -27,16 +27,16 @@ class StableDiffusion:
         
     def initialize_model(self,mode=0):
         model = ''
-        if "stable-diffusion-v1.4" in os.listdir("models"):
-            model = StableDiffusionPipeline.from_pretrained('CompVis/stable-diffusion-v-1-4-original')
+        if "stable-diffusion-v1-4" in os.listdir("models"):
+            model = StableDiffusionPipeline.from_pretrained('models/stable-diffusion-v1-4')
         else:
             model = StableDiffusionPipeline.from_pretrained(
-                'CompVis/stable-diffusion-v-1-4-original',
+                'CompVis/stable-diffusion-v1-4',
                 use_auth_token=TOKEN,
                 revision='fp16',
                 torch_dtype=torch.float16,
             )
-            model.save_pretrained('CompVis/stable-diffusion-v-1-4-original')
+            model.save_pretrained('models/stable-diffusion-v1-4')
 
         # you can set the device on 0 ou 1, create LoadBalancer
         device = torch.device("cuda:"+str(self.gpu_loadbalance()) if torch.cuda.is_available() else "cpu")
@@ -56,7 +56,7 @@ class StableDiffusion:
             for r in range(DEFAULT_IMG_QTD):
                 output = model_intern(
                     prompt,
-                    num_inference_steps=50,           # diffusion iterations default 50
+                    num_inference_steps=100,           # diffusion iterations default 50
                     guidance_scale=7.5,               # adherence to text, default 7.5
                     width=int(resolution_s[0]),       # image width, default 512
                     height=int(resolution_s[1])       # image height, default 512
